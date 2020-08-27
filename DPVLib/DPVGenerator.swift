fc0d8f7eb10f601af8e1776167e31cd801d3f532
Lucas <lucasramos.m@gmail.com>
2020-08-27 01:02:54 -0300

    Versao 1.2.3

--- DPVLib/DPVGenerator.swift
+++ DPVLib/DPVGenerator.swift
@@ -10,7 +10,7 @@ import Foundation
 
 open class DPVGenerator {
     
-    public let instance = DPVGenerator()
+    public static let instance = DPVGenerator()
     
     open var lastGeneratedDPVInfo:DPVInfo?
     
